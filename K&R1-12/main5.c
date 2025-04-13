5642ea9b79d985f5c2ed92a6016ff01f0abcdfa1
John4155 <joh435n@gmail.com>
2025-04-13 11:24:52 +0200

    Create main5.c

---
+++ K&R1-12/main5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main()
+ {
+ float fahr, celsius;
+ fahr = 300;
+ int i = 0;
+ while (fahr >= i) 
+ {
+ celsius = (5.0/9.0) * (fahr-32.0);
+ printf("%3.0f %6.1f\n", fahr, celsius);
+ fahr = fahr-20;
+ }
+ }
