f799622d8a34aaf576cc0b40cd0d060a67ed5a0f
John4155 <joh435n@gmail.com>
2025-04-20 11:18:02 +0200

    Update main15.c

--- K&R13-24/main15.c
+++ K&R13-24/main15.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int celcius1(int fahr)
+{
+    int celcius = 0;
+    celcius = 5 * (fahr-32) / 9;
+    return celcius;  
+}
+
+int main()
+{
+    int step = 20;
+    int lower = 0;
+    int upper = 300;
+    int fahr = lower;
+    while(fahr <= upper)
+    {
+        printf ("%d\t%d \n", fahr, celcius1(fahr));
+        fahr = fahr + step;
+    }
+
+}
