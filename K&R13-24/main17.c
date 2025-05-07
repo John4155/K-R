371e77683ad4109980ac43931d34ca0005a26b82
Daniil <joh435n@gmail.com>
2025-05-07 17:09:56 +0200

    Update main17.c

--- K&R13-24/main17.c
+++ K&R13-24/main17.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+#define LS 1000 // maximal thousand symbols in one array(line)
+int main()
+{
+    int i = 0;
+    int c;
+    char line[LS]; // our line that can get only 1000 symbols
+    while ((c = getchar()) != EOF) 
+    {
+        line[i] = c; // copy every symbol that wrote the user to array
+        i++;
+        if (c == '\n' ) // if we find new line
+        {
+            
+            for(int j = 0; j < i && i >= 80; j++) // we printing every symbol.
+        {
+            printf("%c", line[j]);
+        }
+        i = 0;
+        }
+    }
+    return 0;
+}
