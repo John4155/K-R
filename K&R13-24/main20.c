0b9c03bed49e8d1e48ee9884f13c05d40067d387
43Daniil <cg0mlq@gmail.com>
2025-06-23 22:16:45 +0200

    Update main20.c

--- K&R13-24/main20.c
+++ K&R13-24/main20.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+#define TAB_WIDTH 8
+
+int main() 
+{
+    int c;
+    int pos = 0; 
+
+    while ((c = getchar()) != EOF) 
+    {
+        if (c == '\t') 
+        {
+        int spaces = TAB_WIDTH - (pos % TAB_WIDTH); 
+            for (int i = 0; i < spaces; i++) 
+            {
+            putchar(' ');
+            pos++;
+            }
+        } 
+        else 
+        {
+            putchar(c);
+            pos++;
+            if(c == '\n')
+            {
+                pos = 0;
+            }
+        }
+    }
+
+    return 0;
+}
